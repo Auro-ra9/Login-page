373155af6dba5a9f211805cb01eabc8638853b6e
Auro-ra9 <ashnaahammed9@gmail.com>
2024-04-05 16:29:00 +0530

    project added

---
+++ server.js
@@ -0,0 +1,49 @@
+
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const { v4: uuidv4 } = require("uuid");
+const nocache = require("nocache");
+const router = require('./router');
+
+
+const app = express();
+
+const port = process.env.port || 3000;
+
+
+
+app.use(bodyParser.json())
+app.use(nocache());
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+app.set('view engine', 'ejs');
+
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
+
+app.use(session({
+    secret: uuidv4(),
+    resave: false,
+    saveUninitialized: true
+
+}));
+
+app.use('/route', router);
+
+
+app.get('/', (req, res) => {
+    if (req.session.user) {
+        res.redirect('/route/dashboard');
+    } else {
+        res.render('base', { title: "login system" });
+    }
+})
+
+app.listen(port, () => {
+
+    console.log(`listening to the server on http://localhost:${port}`);
+})
